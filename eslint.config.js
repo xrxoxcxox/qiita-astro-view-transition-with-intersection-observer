1cde389b00e2bb1a8e5d036fda8d3930eae24615
Keisuke Watanuki <watanuki.keisuke@gmail.com>
2024-12-09 01:07:11 +0900

    Add ESLint and Prettier configurations

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginAstro from "eslint-plugin-astro";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,astro}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  eslintConfigPrettier,
+];
